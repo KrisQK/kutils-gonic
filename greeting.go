0f93a85348317459e232d77b024c3fac072d42b5
Kris Qikun Li - Mac <qikunli8866+mac@gmail.com>
2024-11-07 01:28:56 +1300

    greeting: changes for v0.1.2

--- greeting.go
+++ greeting.go
@@ -17,6 +17,45 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, errors.New("empty names")
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+// func Hellos(names []string) ([]string, error) {
+// 	if len(names) == 0 { // 检查 names 是否为空
+// 		return nil, errors.New("empty names")
+// 	}
+
+// 	messages := []string{}
+// 	for i := 0; i < len(names); i++ {
+// 		// 正确使用 fmt.Sprintf 进行格式化
+// 		message := fmt.Sprintf(randomFormat(), names[i])
+// 		messages = append(messages, message)
+// 	}
+// 	return messages, nil
+// }
+
+// func Hellos(names []string) (string, error) {
+// 	if names == nil {
+// 		return names, errors.New("empty names")
+// 	}
+
+// 	messages := []string{}
+// 	for i := 0; i < len(names); i++ {
+// 		message := fmt.Sprint(randomFormat(), names[i])
+// 		messages = append(messages, message)
+// 	}
+// 	return messages, nil
+// }
+
 // randomFormat returns one of a set of greeting messages. The returned
 // message is selected at random.
 func randomFormat() string {
